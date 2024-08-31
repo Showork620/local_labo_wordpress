0ee42d4001b984ea3016a3502ea3e17ec6e955e4
showork620 <showork620>
2024-08-31 18:54:10 +0900

    feat: カラーパレットの初期実装（html,css）

--- html/wp-content/themes/local_labo/index.php
+++ html/wp-content/themes/local_labo/index.php
@@ -8,10 +8,22 @@
 
 get_header();
 ?>
-
-	<div>
-		<main>テストです</main>
+	<div class="l-page">
+		<header>
+			<h1>hello</h1>
+			<p>キー: <span id="js-key-name">なし</span></p>
+			<p>番号: <span id="js-key-number">なし</span></p>
+		</header>
+		<main>
+			<div class="p-keyboard-pallet">
+				<?php // 12*4 のbox要素を配置. ?>
+				<?php for ($i = 1; $i <= 48; $i++) : ?>
+					<div class="p-keyboard-pallet__cell js-pallet-cell" data-key-number="<?php echo $i ?>" data-pressed-count="0">
+					</div>
+				<?php endfor; ?>
+			</div>
+		</main>
+		<footer></footer>
 	</div>
-
 <?php
 get_footer();
